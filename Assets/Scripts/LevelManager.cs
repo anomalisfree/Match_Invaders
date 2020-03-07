fc18f4ee24e43692302fd7a65b96fd56ebfc933b
anomalisfree <anomalisfree@gmail.com>
2020-03-07 22:10:47 +0300

    Add project

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary> Manages the state of the level </summary>
+public class LevelManager : MonoBehaviour
+{
+    public int Score { get; private set; }
+    
+    public void IncrementScore()
+    {
+        Score++;
+    }
+
+    public void Reset()
+    {
+        Score = 0;
+        // reset logic
+    }
+}
---
+++ Assets/Scripts/MainCharacter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class MainCharacter : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed;
+        } else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed;
+        }
+    }
+}
