c4d27d8eb39b5abfc3198dc8ffe4afdac28188a6
anomalisfree <anomalisfree@gmail.com>
2020-03-09 01:53:09 +0300

    Enemies logic.

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    [SerializeField] private int health;
+    [SerializeField] private GameObject deadPrefab;
+
+    private Transform _transform;
+
+    private void Start()
+    {
+        _transform = transform;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.GetComponent<Bullet>() != null)
+        {
+            health--;
+            Destroy(other.gameObject);
+        }
+        
+        if (other.gameObject.GetComponent<EnemyItem>() != null)
+        {
+            health = 0;
+        }
+        
+        if(health <= 0)
+        {
+            var deadObject = Instantiate(deadPrefab, _transform.position, _transform.rotation);
+            deadObject.GetComponent<SpriteRenderer>().color = Color.green;
+            Destroy(deadObject, 0.2f);
+            
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/MainCharacter.cs
+++ Assets/Scripts/MainCharacter.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class MainCharacter : MonoBehaviour
 {
@@ -6,11 +7,31 @@ public class MainCharacter : MonoBehaviour
     [SerializeField] private Transform bulletCreator;
     [SerializeField] private GameObject bullet;
 
+    private Transform _transform;
+
+    private void Start()
+    {
+        _transform = transform;
+    }
+
     private void Update()
     {
         if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1f)
         {
-            transform.position += Vector3.right * (Input.GetAxis("Horizontal") * speed * Time.deltaTime);
+            var position = _transform.position;
+            
+            position += Vector3.right * (Input.GetAxis("Horizontal") * speed * Time.deltaTime);
+
+            if ( position.x < -12)
+            {
+               position = new Vector3(-12,  position.y,  position.z);
+            }
+            else if ( position.x > 12)
+            {
+                position = new Vector3(12, position.y,  position.z);
+            }
+            
+            _transform.position = position;
         }
 
         if (Input.GetButtonDown("Fire1"))
